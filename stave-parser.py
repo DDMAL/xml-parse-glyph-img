53ca7e119af4589733e1eef16f92eb841cbf6121
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-05-31 16:13:24 -0400

    Comment cleanup

--- stave-parser.py
+++ stave-parser.py
@@ -118,36 +118,3 @@ for index, stave_coord in enumerate(final_stave_coords):
             stave_coord[0]-60:stave_coord[1]+60,
             x_start-30:x_end+30
         ])
-
-# uly_temp = 0
-# index = 0
-#
-# for staff in staff_root.findall('staves'):
-#     uly = int(staff.find('bounding_box').find('uly').text)
-#     ulx = int(staff.find('bounding_box').find('ulx').text)
-#     ncols = int(staff.find('bounding_box').find('ncols').text)
-#     nrows = int(staff.find('bounding_box').find('nrows').text)
-#
-#     print(uly, ulx)
-#
-#     # if abs(uly - uly_temp) > 200:
-#     cv2.imwrite(f'./staff_boxes/staff_{ index }_bb.png',
-#         img[uly:uly+nrows, ulx:ulx+ncols])
-#     index += 1
-
-
-
-
-#
-#     uly_temp = uly
-
-# x_len = 0
-# y_len = 0
-#
-# for stave_index, stave in enumerate(staff_root.findall('staves')):
-#     print(stave_index)
-#     for stave_next in staff_root.findall('staves')[stave_index+1:]:
-#         if int(stave_next.find('bounding_box').find('ulx').text) > int(stave.find('bounding_box').find('ulx').text):
-#             print('yeet!')
-#         else:
-#             break
