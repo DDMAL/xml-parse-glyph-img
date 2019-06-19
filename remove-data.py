1c54c028379985e9bb25eb8f24e898be65a2c26d
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-06-19 18:01:44 -0400

    Incorporated cli control of data removal

--- remove-data.py
+++ remove-data.py
@@ -1,3 +1,11 @@
 import os
 
-os.system('rm -rf position_test position_train position_test.zip position_train.zip position_test.txt position_train.txt')
+print('What data should be removed? Training (0), Testing (1), All (2) ')
+choice = int(input().strip())
+
+if choice == 0:
+    os.system('rm -rf position_train position_train.zip position_train.txt')
+elif choice == 1:
+    os.system('rm -rf position_test position_test.zip position_test.txt')
+elif choice == 2:
+    os.system('rm -rf position_test position_train position_test.zip position_train.zip position_test.txt position_train.txt')
