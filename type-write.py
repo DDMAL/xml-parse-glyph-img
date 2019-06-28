5f2f7cf803f904ae490f1bc5707205960162d23d
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-06-28 15:57:15 -0400

    Added cases for making type classification more efficient

--- type-write.py
+++ type-write.py
@@ -28,13 +28,42 @@ for glyph in stave_root.find('glyphs'):
     nrows = int(glyph.get('nrows'))
     ncols = int(glyph.get('ncols'))
 
-    type_class = glyph.find('type')
+    type_class = glyph.find('ids').find('id')
 
-    cv.imshow('image', image[uly:uly+nrows,ulx:ulx+ncols])
+    cv.imshow('image', image[uly-100:uly+nrows+100,ulx-20:ulx+ncols+20])
     cv.waitKey(2)
     print('Type: ')
     type = str(input().strip())
-    type_class.set('name', type)
+    if type == '':
+        type_class.set('name', 'punctum')
+    elif type == 'i':
+        type_class.set('name', 'inclinatum')
+    elif type == 'v':
+        type_class.set('name', 'virga')
+    elif type == 'c':
+        type_class.set('name', 'custos')
+    elif type == 'cc':
+        type_class.set('name', 'c_clef')
+    elif type == 'fc':
+        type_class.set('name', 'f_clef')
+    elif type == 'p2':
+        type_class.set('name', 'podatus2')
+    elif type == 'p3':
+        type_class.set('name', 'podatus3')
+    elif type == 'p4':
+        type_class.set('name', 'podatus4')
+    elif type == 'p5':
+        type_class.set('name', 'podatus5')
+    elif type == 'o2':
+        type_class.set('name', 'oblique2')
+    elif type == 'o3':
+        type_class.set('name', 'oblique3')
+    elif type == 'o4':
+        type_class.set('name', 'oblique4')
+    elif type == 'o5':
+        type_class.set('name', 'oblique5')
+    else:
+        type_class.set('name', type)
     # if inc == 20:
     #     break
     # inc += 1
